a08a720a688038406e04987624c53fe99e58cc9f
Sarmitha <156922837+sarmi2325@users.noreply.github.com>
2025-05-19 22:30:42 +0530

    Update facial_emotion.py

--- Computer_vision/facial_emotion.py
+++ Computer_vision/facial_emotion.py
@@ -8,9 +8,16 @@ url='IP_WEBCAM_URL/IMAGE.jpg'
 
 while True:
     imgpath=urllib.request.urlopen(url)
+
+    
+    '''imgpath.read() reads the uploaded image as binary data.
+       bytearray(...) turns it into a mutable byte array.
+       np.array(..., dtype=np.uint8) converts it into a NumPy array of unsigned 8-bit integers (this is the standard format for image data).'''
     imgnp=np.array(bytearray(imgpath.read()),dtype=np.uint8)
     frame=cv2.imdecode(imgnp,-1)
     frame=imutils.resize(frame,width=450)
+
+    
     try:
         result = DeepFace.analyze(frame, actions=['emotion'], enforce_detection=False)
         emotion = result[0]['dominant_emotion']
